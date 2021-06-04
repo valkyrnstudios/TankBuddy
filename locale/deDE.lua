a2cf4ad3b21f65c838c665197f5dfdf8cbeae794
joel.garboden <sabre@valkyrnstudios.net>
2021-06-03 22:47:46 -0700

    Cleanup locales

--- locale/deDE.lua
+++ locale/deDE.lua
@@ -6,104 +6,79 @@ if not L then return end
 -- by StarDust, Lorni and Nelson from Malfurion EU
 -- last changed: June 15th, 2008
 -- NB! Partial translation only!
-L["TankBuddy"] = "Tank Buddy";
-L["EnableTankBuddy"] = "Tank Buddy aktivieren";
-L["Raid"] = "Schlachtzug";
-L["Party"] = "Gruppe";
+
 L["Alone"] = "Allein";
 L["AnnouncementChannel"] = "Nachrichtenkanal";
 L["AnnouncementTexts"] = "Benachrichtigungstext";
-L["SetCustomChannel"] = "Eigener Kanal";
-L["EnterChannelName"] = "Kanalname oder Nummer angeben:";
-L["Close"] = "Schlie\195\159en";
-L["Help"] = "Hilfe";
-L["Test"] = "Testen";
-L["Copy"] = "Kopieren";
-L["Paste"] = "Einfügen";
-L["General"] = "Allgemein";
-L["Taunt"] = "Spott";
-L["MB"] = "Spöttischer Schlag";
-L["LS"] = "Letztes Gefecht";
-L["SW"] = "Schildwall";
-L["LG"] = "Lebensspendender Edelstein";
-L["CS"] = "Herausforderungsruf";
-L["Growl"] = "Knurren";
 L["CR"] = "Herausforderndes Gebrüll";
-L["RD"] = "Rechtschaffene Verteidigung";
-L["RF"] = "Zorn der Gerechtigkeit";
-L["Parry"] = "Parry";
-L["EnterNewText"] = {
-    ["Taunt"] = "Neuer Text wenn Spott wiederstanden wird:",
-    ["MB"] = "Neuer Text wenn Spöttischer Schlag verfehlt:",
-    ["LS"] = "Neuer Text wenn Letztes Gefecht aktiviert wird:",
-    ["SW"] = "Neuer Text wenn Schildwall aktiviert wird:",
-    ["LG"] = "Neuer Text für Benutzung von Lebensspendender Edelstein:",
-    ["CS"] = "Neuer Text wenn Herausforderndes Gebrüll aktiviert wird:",
-    ["Growl"] = "Neuer Text wenn Knurren wiederstanden wird:",
-    ["CR"] = "Neuer Text wenn Herausforderndes Gebrüll aktiviert wird:",
-    ["RD"] = "Neuer Text wenn Rechtschaffene Verteidigung wiederstanden wird:"
-}
-L["EnterNewMBRecoveryText"] =
-    "Neuer Text für Wiederherstellung von verfehltem Spott:";
-L["RemoveBuffs"] = "Buff Entfernung";
-L["RemoveBuffsAlways"] = "Entferne Buffs die folgendes beinhalten:";
+L["CS"] = "Herausforderungsruf";
 L["DisableInBG"] = "Deaktiviert in Schlachtfeldern";
-L["RemoveBuffsDefensive"] = "Nur in Verteidigungshaltung";
-L["RemoveBuffsBear"] = "Nur in Bärform";
-L["RemoveBuffsPally"] = "Nur mit Zorn der Gerechtigkeit:";
+L["Done"] = "getan!";
 L["EnableMBRecovery"] =
     "Ankündigung für Wiederherstellung von verfehltem Spott";
+L["EnableTankBuddy"] = "Tank Buddy aktivieren";
+L["EnterNewMBRecoveryText"] =
+    "Neuer Text für Wiederherstellung von verfehltem Spott:";
+L["FR"] = "Teufelswut"
+L["General"] = "Allgemein";
+L["Growl"] = "Knurren";
+L["Help"] = "Hilfe";
+L["LG"] = "Lebensspendender Edelstein";
+L["LS"] = "Letztes Gefecht";
+L["MB"] = "Spöttischer Schlag";
+L["output_buffremoved"] = " removed, matched "; -- As in ["Greater Blessing of Salvation" removed, matched "Salvation"]
+L["Party"] = "Gruppe";
+L["Raid"] = "Schlachtzug";
+L["RD"] = "Rechtschaffene Verteidigung";
+L["RF"] = "Zorn der Gerechtigkeit";
+L["salvation"] = "Segen der Rettung";
+L["SW"] = "Schildwall";
+L["TankBuddy"] = "Tank Buddy";
+L["Taunt"] = "Spott";
+L["Test"] = "Testen";
 
 L["IntroText"] =
     "Danke für die Benutzung von Tank Buddy, ehemals bekannt als Taunt Buddy.\nTaunt Buddy wurde ursprünglich von Artun Subasientwickelt, aber seit er die Entwicklung eingestellt hat, übernahm Kolthor von Doomhammer EU.\n\nAuf der rechten Seite sind eine Anzahl von Tabs, abhängig von deiner Klasse. Jedes Tab hat Optionen für Ankündigungsnachrichten und Kanäle in die benachrichtigt werden soll unter bestimmten Bedingungen."
 L["HelpText"] =
     "Buff Entfernung:\nUm unerwünschte Buffs zu entfernen gibt es zwei Möglichkeiten: Immer, oder nur in Verteidigungshaltung / Bärform / Zorn der Gerechtigkeit.\nTank Buddy wird nach Buffs suchen, die den angegebenen Text enthalten. Um nach mehreren Buffs zu suchen, einfach die Suchworte mit Komma (,) trennen. Hinweis: Du musst nicht den gesamten Buff-Text eingeben, ein Teilstück reicht. Beispiel: \"Intelligenz, Weisheit\" wird nach allen Buffs suchen, die das Wort \"Intelligenz\" oder \"Weisheit\" beinhalten und diese entfernen. Dies trifft auf \"Arkane Intelligenz\" zu und auf \"Gebet der Weisheit\" , aber auch auf Rollen der Intelligenz und Weisheits Buffs und alle anderen Buffs, die \"Intelligenz\" oder \"Weisheit\" in ihrem Namen haben.\n\nTabs:\nDie ganz linke Spalte der Auswahlkästen bestimmt in welche Kanäle benachrichtigt wird, wenn du in einem Raid bist. Die mittlere wenn du in einer Gruppe bist und die ganz rechte wenn du allein unterwegs bist.\nWenn du ein \"Eigener\"-kästchen anwählst wird ein Fenster aufgehen in dem du einen oder mehrere Kanäle eingeben kannst, entweder als Kanalname oder mit der Nummer. Trenne die Kanäle mit einem Leerzeichen.\nWenn du ein paar Kanäle ausgewählt hast, kannst du sie mit dem \"Kopieren\"-Knopf kopieren, dann einen anderen Tab auswählen und mit dem \"Einfügen\"-Knopf die gleiche Wahl wieder einfügen.\nAchtung! Dies wird alle vorherigen Eingaben mit den kopierten Kanälen überschreiben.\nDu kannst in der Box unten eine Nachricht eingeben und dabei folgende Variablen verwenden:\n\nSpott, Knurren and Herausforderndes Gebrüll:\n$tn: Target Name (dasselbe wie %t)\n$tl: TargetLevel\n$tt: Ziel Typ (Humanoid, Tier, Dämon etc.)\n$ttn: Target's Target Name\n$ttl: Target's Target Level\n$ttt: Target's Target Typ\n\nSchildwall, Herausforderungsruf, Herausforderndes Gebrüll, Letztes Gefecht und Lebensspendender Edelstein:\n$sec: Dauer der Fähigkeit in Sekunden\n\nLetztes Gefecht Lebensspendender Edelstein:\n$hp: Anzahl der zusätzlichen Lebenspunkte durch diese Fähigkeit";
 
-L["Channel_Ctraid"] = "CTRaid";
-L["Channel_RaidWarning"] = "Schlachtzugswarnung";
-L["Channel_Raid"] = "Schlachtzug";
-L["Channel_Guild"] = "Gilde";
-L["Channel_Party"] = "Gruppe";
-L["Channel_Yell"] = "Schreien";
-L["Channel_Say"] = "Sagen";
-L["Channel_Custom"] = "Eigener";
+L["RemoveBuffs"] = {
+    ["Title"] = "Buff Entfernung",
+    ["Always"] = "Entferne Buffs die folgendes beinhalten:",
+    ["DRUID"] = "Nur in Bärform",
+    ["WARRIOR"] = "Nur in Verteidigungshaltung",
+    ["PALADIN"] = "Nur mit Zorn der Gerechtigkeit:"
+}
+
+L["Channel"] = {
+    ["Ctraid"] = "CTRaid",
+    ["Emote"] = "Emote",
+    ["Party"] = "Gruppe",
+    ["Raid"] = "Schlachtzug",
+    ["RaidWarning"] = "Schlachtzugswarnung"
+}
 
 L["defaultText"] = {
-    ["Taunt"] = "- Mein Spott wurde von $tn widerstanden -",
-    ["MB"] = "- Mein Spöttischer Schlag hat $tn verfehlt -",
-    ["LS"] = "- Auf ins letzte Gefecht! In $sec Sekunden verliere ich $hpHP! -",
-    ["SW"] = "- Schildwall ist raus, ich bekomme nur 75% Schaden für $sec Sekunden! -",
-    ["LG"] = "- Ich habe Lebensspendender Edelstein aktiviert! In $sec Sekunden verliere ich $hpHP! -",
+    ["CR"] = "- Herrausforderndes Gebrüll ist raus! Ich brauche Heilung für die nächsten $sec Sekunden! -",
     ["CS"] = "- Herausforderungsruf ist raus! Ich brauche Heilung für die nächsten $sec Sekunden! -",
     ["Growl"] = "- Mein Knurren wurde von $tn widerstanden -",
-    ["CR"] = "- Herrausforderndes Gebrüll ist raus! Ich brauche Heilung für die nächsten $sec Sekunden! -",
-    ["RD"] = "- Mein Rechtschaffene Verteidigung wurde von $tn widerstanden -"
+    ["LG"] = "- Ich habe Lebensspendender Edelstein aktiviert! In $sec Sekunden verliere ich $hpHP! -",
+    ["LS"] = "- Auf ins letzte Gefecht! In $sec Sekunden verliere ich $hpHP! -",
+    ["MB"] = "- Mein Spöttischer Schlag hat $tn verfehlt -",
+    ["RD"] = "- Mein Rechtschaffene Verteidigung wurde von $tn widerstanden -",
+    ["recovery"] = "- Mein Spöttischer Schlag hat meinen widerstandenen Spott wieder hergestellt! -",
+    ["SW"] = "- Schildwall ist raus, ich bekomme nur 75% Schaden für $sec Sekunden! -",
+    ["Taunt"] = "- Mein Spott wurde von $tn widerstanden -"
 }
-L["defaultText_recovery"] =
-    "- Mein Spöttischer Schlag hat meinen widerstandenen Spott wieder hergestellt! -";
 
-L["salvation"] = "Segen der Rettung";
-L["intellect"] = "Intelligenz";
-L["wisdom"] = "Weisheit";
-L["Taunt"] = "Spott";
-L["MB"] = "Spöttischer Schlag";
-L["LS"] = "Letztes Gefecht";
-L["SW"] = "Schildwall";
-L["LG"] = "Lebensspendender Edelstein";
-L["CS"] = "Herausforderungsruf";
-L["Growl"] = "Knurren";
-L["CR"] = "Herausforderndes Gebrüll";
-L["RD"] = "Rechtschaffene Verteidigung";
-L["RF"] = "Zorn der Gerechtigkeit";
-L["FR"] = "Teufelswut"
-L["Parry"] = "Parry";
-
-L["output_buffremoved"] = " removed, matched "; -- As in ["Greater Blessing of Salvation" removed, matched "Salvation"]
-L["output_startup"] = " geladen. Gib /TB Help ein um eine Hilfe anzuzeigen.";
-L["output_alreadyOff"] = "Tank Buddy ist bereits deaktiviert.";
-L["output_alreadyOn"] = "Tank Buddy ist bereits aktiviert.";
-L["output_off"] = "Tank Buddy ist aktiv.";
-L["output_on"] = "Tank Buddy ist nicht aktiv.";
-
-L["cmd_on"] = "an";
-L["cmd_off"] = "aus";
+L["EnterNewText"] = {
+    ["CR"] = "Neuer Text wenn Herausforderndes Gebrüll aktiviert wird:",
+    ["CS"] = "Neuer Text wenn Herausforderndes Gebrüll aktiviert wird:",
+    ["Growl"] = "Neuer Text wenn Knurren wiederstanden wird:",
+    ["LG"] = "Neuer Text für Benutzung von Lebensspendender Edelstein:",
+    ["LS"] = "Neuer Text wenn Letztes Gefecht aktiviert wird:",
+    ["MB"] = "Neuer Text wenn Spöttischer Schlag verfehlt:",
+    ["RD"] = "Neuer Text wenn Rechtschaffene Verteidigung wiederstanden wird:",
+    ["SW"] = "Neuer Text wenn Schildwall aktiviert wird:",
+    ["Taunt"] = "Neuer Text wenn Spott wiederstanden wird:"
+}
--- locale/enUS.lua
+++ locale/enUS.lua
@@ -9,96 +9,78 @@
     - by Artun Subasi and Kolthor
 --]]
 
-L["TankBuddy"] = "Tank Buddy";
-L["EnableTankBuddy"] = "Enable Tank Buddy";
-L["Raid"] = "Raid";
-L["Party"] = "Party";
 L["Alone"] = "Alone";
 L["AnnouncementChannel"] = "Announcement Channel";
 L["AnnouncementTexts"] = "Announcement Texts";
-L["SetCustomChannel"] = "Set Custom Channel";
-L["EnterChannelName"] = "Enter channel name or number:";
-L["Close"] = "Close";
-L["Help"] = "Help";
-L["Test"] = "Test";
-L["Copy"] = "Copy";
-L["Paste"] = "Paste";
-L["General"] = "General";
-L["Taunt"] = "Taunt";
-L["MB"] = "Mocking Blow";
-L["SW"] = "Berserker Rage"; -- "Shield Wall";
-L["LG"] = "Lifegiving Gem";
-L["CS"] = "Challenging Shout";
-L["Growl"] = "Growl";
 L["CR"] = "Challenging Roar";
-L["RD"] = "Righteous Defense";
-L["RF"] = "Righteous Fury";
-L["Parry"] = "Parry";
-L["LS"] = "Last Stand";
-L["FR"] = "Fel Rage";
-
-L["EnterNewText"] = {
-    ["Taunt"] = "Enter new announcement text for resisted taunts:",
-    ["MB"] = "Enter new announcement text for failed mocking blows:",
-    ["LS"] = "Enter new announcement text for using last stand:",
-    ["SW"] = "Enter new announcement text for using shield wall:",
-    ["LG"] = "Enter new announcement text for using lifegiving gem:",
-    ["CS"] = "Enter new announcement text for using challenging shout:",
-    ["Growl"] = "Enter new announcement text for resisted growls:",
-    ["CR"] = "Enter new announcement text for using challenging roar:",
-    ["RD"] = "Enter new announcement text for resisted Righteous Defense:",
-    ["Parry"] = "Enter new announcement text for boss parry:"
-}
+L["CS"] = "Challenging Shout";
+L["DisableInBG"] = "Disable in battlegrounds";
+L["Done"] = "done!";
+L["EnableMBRecovery"] = "Enable announcement for recovery of failed taunts";
+L["EnableTankBuddy"] = "Enable Tank Buddy";
 L["EnterNewMBRecoveryText"] =
     "Enter new announcement text for recovered taunts:";
-L["RemoveBuffs"] = "Buff removal";
-L["RemoveBuffsAlways"] = "Always remove buffs containing:";
-L["DisableInBG"] = "Disable in battlegrounds";
-L["RemoveBuffsDefensive"] = "Only defensive stance:";
-L["RemoveBuffsBear"] = "Only in bear form:";
-L["RemoveBuffsPally"] = "Only when Righteous Fury is active:";
-L["EnableMBRecovery"] = "Enable announcement for recovery of failed taunts";
-L["ParryWarnings"] = "Enable parry warning whispers";
+L["FR"] = "Fel Rage";
+L["General"] = "General";
+L["Growl"] = "Growl";
+L["Help"] = "Help";
+L["LG"] = "Lifegiving Gem";
+L["LS"] = "Last Stand";
+L["MB"] = "Mocking Blow";
+L["output_buffremoved"] = " removed, matched "; -- As in ["Greater Blessing of Salvation" removed, matched "Salvation"]
+L["Party"] = "Party";
+L["Raid"] = "Raid";
+L["RD"] = "Righteous Defense";
+L["RF"] = "Righteous Fury";
+L["salvation"] = "Salvation";
+L["SW"] = "Shield Wall";
+L["TankBuddy"] = "Tank Buddy";
+L["Taunt"] = "Taunt";
+L["Test"] = "Test";
 
 L["IntroText"] =
     "Thank you for using Tank Buddy, formerly known as Taunt Buddy.\nTaunt Buddy was originally created by Artun Subasi, but since he stopped development, Kolthor from Doomhammer EU took over. Updated for 2.4 combatlog & Paladin support by Raeldar.\n\nTo the right there are a number of tabs, depending on your class. Each tab has options for announcement message, and channels to announce to under given circumstances."
 L["HelpText"] =
     "Buff removal:\nTo remove unwanted buffs, there are two options; Always, and only in defensive stance/bear form/Righteous Fury.\nTank Buddy will look for buffs containing any of the text specified. To look for several buffs, just seperate with a comma (,). Note! You don't need to type the entire name of the buff, just portions. Example: \"Intellect, Spirit\" will look for any buffs that has the word \"Intellect\" or \"Spirit\" in it, and remove them. This will match \"Arcane Intellect\" and \"Prayer of Spirit\", but also the Scroll of Intellect and Sprit buffs and any other buff that has \"Intellect\" or \"Spirit\" in it's name.\n\nTabs:\nThe leftmost column of checkboxes controls which channels to send the announcement message to, if you are in a raid. The middle column, if you are in a party, and the rightmost column, if you are alone.\nIf you check any of the \"Custom\"-boxes, a window will appear where you can type in the custom channel(s), either by channel name or number. To specify more than one channel, seperate with a space.\nWhen you have selected some channels, you can click the \"Copy\"-button to copy your selection, and then click another tab, and click the \"Paste\"-button to use the same selection there.\nNote! This will overwrite any custom channels you might have typed, with the ones from the copied source.\nYou can specify a message in the editbox at the bottom, and use the variables listed below.\nIf you select \"Enable announcement for recovery of failed taunts\" in the taunt-tab, you can type a message to announce in the event that you successfully hit with a Mocking Blow, after a failed taunt. Note! This will only work until Taunt is no longer on cooldown, and only if your current target has the same name and level as the one that resisted the taunt.\n\nVariables:\nTaunt, Growl and Mocking Blow:\n$tn: Target Name (Same as %t)\n$tl: Target Level\n$tt: Target Type (Humanoid, Beast, Demon etc.)\n$ttn: Target's Target Name\n$ttl: Target's Target Level\n$ttt: Target's Target Type\n\nShield Wall, Challenging Shout, Challenging Roar, Last Stand and Lifegiving Gem:\n$sec: Duration of ability in seconds\n\nLast Stand and Lifegiving Gem:\n$hp: Amount of hitpoints gained by the ability.";
 
-L["Channel_Ctraid"] = "CTRaid";
-L["Channel_RaidWarning"] = "Raid Warning";
-L["Channel_Raid"] = "Raid";
-L["Channel_Guild"] = "Guild";
-L["Channel_Party"] = "Party";
-L["Channel_Yell"] = "Yell";
-L["Channel_Say"] = "Say";
-L["Channel_Custom"] = "Custom";
+L["RemoveBuffs"] = {
+    ["Title"] = "Buff removal",
+    ["Always"] = "Always remove buffs containing:",
+    ["DRUID"] = "Only in bear form:",
+    ["WARRIOR"] = "Only defensive stance:",
+    ["PALADIN"] = "Only when Righteous Fury is active:"
+}
+
+L["Channel"] = {
+    ["Ctraid"] = "CTRaid",
+    ["RaidWarning"] = "Raid Warning",
+    ["Raid"] = "Raid",
+    ["Party"] = "Party"
+}
 
 L["defaultText"] = {
-    ["Taunt"] = "- My Taunt has been resisted by $tn! -",
-    ["MB"] = "- My Mocking Blow failed against $tn! -",
-    ["LS"] = "- I activated Last Stand! In $sec seconds I will lose $hpHP! -",
-    ["SW"] = "- I activated Shield Wall and will be taking 75% less damage for $sec seconds! -",
-    ["LG"] = "- I activated Lifegiving Gem! In $sec seconds I will lose $hpHP! -",
+    ["CR"] = "- I activated Challenging Roar! I will need a lot of healing for $sec seconds! -",
     ["CS"] = "- I activated Challenging Shout! I will need a lot of healing for $sec seconds! -",
     ["Growl"] = "- My Growl has been resisted by $tn! -",
-    ["CR"] = "- I activated Challenging Roar! I will need a lot of healing for $sec seconds! -",
+    ["LG"] = "- I activated Lifegiving Gem! In $sec seconds I will lose $hpHP! -",
+    ["LS"] = "- I activated Last Stand! In $sec seconds I will lose $hpHP! -",
+    ["MB"] = "- My Mocking Blow failed against $tn! -",
+    ["Parry"] = " - Your $skill has been parried, get behind the boss! = ",
     ["RD"] = "- My Righteous Defense has been resisted by $tn! -",
-    ["Parry"] = " - Your $skill has been parried, get behind the boss! = "
+    ["recovery"] = "- My Mocking Blow RECOVERED my resisted taunt! -",
+    ["SW"] = "- I activated Shield Wall and will be taking 75% less damage for $sec seconds! -",
+    ["Taunt"] = "- My Taunt has been resisted by $tn! -"
 }
-L["defaultText_recovery"] = "- My Mocking Blow RECOVERED my resisted taunt! -";
 
-L["salvation"] = "Salvation";
-L["intellect"] = "Intellect";
-L["wisdom"] = "Wisdom";
-
-L["output_buffremoved"] = " removed, matched "; -- As in ["Greater Blessing of Salvation" removed, matched "Salvation"]
-L["output_startup"] = " loaded. Type /TB for options.";
-L["output_alreadyOff"] = "Tank Buddy is already disabled.";
-L["output_alreadyOn"] = "Tank Buddy is already enabled.";
-L["output_off"] = "Tank Buddy off.";
-L["output_on"] = "Tank Buddy on.";
-
-L["cmd_on"] = "on";
-L["cmd_off"] = "off";
-
+L["EnterNewText"] = {
+    ["CR"] = "Enter new announcement text for using challenging roar:",
+    ["CS"] = "Enter new announcement text for using challenging shout:",
+    ["Growl"] = "Enter new announcement text for resisted growls:",
+    ["LG"] = "Enter new announcement text for using lifegiving gem:",
+    ["LS"] = "Enter new announcement text for using last stand:",
+    ["MB"] = "Enter new announcement text for failed mocking blows:",
+    ["Parry"] = "Enter new announcement text for boss parry:",
+    ["RD"] = "Enter new announcement text for resisted Righteous Defense:",
+    ["SW"] = "Enter new announcement text for using shield wall:",
+    ["Taunt"] = "Enter new announcement text for resisted taunts:"
+}
