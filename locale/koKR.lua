5f635e4c37043c4614e2f2055cb10388a0e19ae5
SabreValkyrn <sabre@valkyrnstudios.net>
2021-08-26 21:00:08 -0700

    Update locale declaration

--- locale/koKR.lua
+++ locale/koKR.lua
@@ -1,4 +1,4 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale("TankBuddy", "enUS", true)
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("TankBuddy", "koKR")
 
 -- by Raeldar, Artun Subasi, and Kolthor
 
